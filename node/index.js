9840a476d7ca2cb60d3d4c0f54a1ae1f991217ab
Ahmad Khalaf <ahmad.khalaf992@gamil.com>
2025-07-22 13:15:18 +0300

    update index.js add other commint

--- node/index.js
+++ node/index.js
@@ -4,6 +4,7 @@ const app = express();
 const json_content=json_file.readFileSync('file.json','utf8');
 app.get('/',(req,res)=> {
 // add this comment file
+//another commint
 	res.send(json_content);
 });
 app.listen(8001,() =>{
