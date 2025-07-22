cf6bb4e8bee18ff125dc0177f4b1a3adde27ecad
Ahmad Khalaf <ahmad.khalaf992@gamil.com>
2025-07-22 13:11:49 +0300

    update index.js

--- node/index.js
+++ node/index.js
@@ -3,6 +3,7 @@ let json_file= require('fs');
 const app = express();
 const json_content=json_file.readFileSync('file.json','utf8');
 app.get('/',(req,res)=> {
+// add this comment file
 	res.send(json_content);
 });
 app.listen(8001,() =>{
